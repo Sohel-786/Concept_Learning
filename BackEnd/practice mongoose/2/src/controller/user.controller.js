39be1df9e1571bcdce23a08397a13008d37e2b4c
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2023-06-09 22:30:22 +0530

    don

--- BackEnd/practice mongoose/2/src/controller/user.controller.js
+++ BackEnd/practice mongoose/2/src/controller/user.controller.js
@@ -2,10 +2,16 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user.model')
 
+router.get('/create', async (req, res) =>{
+    return res.render('addUsers.ejs');
+})
+
 router.post('', async (req, res) =>{
     try{
-        let user = await User.create(req.body);
-        return res.status(201).send(user);
+        // let user = await User.create(req.body);
+        // return res.status(201).send(user);
+
+            return res.send(req.body);
     }catch(err){
         return res.status(400).send(err.message)
     }
--- BackEnd/practice mongoose/2/src/server.js
+++ BackEnd/practice mongoose/2/src/server.js
@@ -4,6 +4,7 @@ const app = express();
 
 const connect = require('./config/db')
 app.use(express.json())
+app.use(express.urlencoded({extended : false}));
 
 app.set('views', path.join(__dirname,'views'));
 app.set('view engine', 'ejs');
