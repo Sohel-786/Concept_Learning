df197e33f5e953d8dd1c617a762d50a8bac43bc0
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2023-07-10 17:06:45 +0530

    practice

--- BackEnd/Modules_exercise/index.js
+++ BackEnd/Modules_exercise/index.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 
+//------------------ Reading files in async way using readFile()
+
 // console.log('Line 1')
 
 // fs.readFile('./txtFiles/input.txt', (err, data) =>{
@@ -11,30 +13,56 @@ const fs = require('fs');
 //     }
 // })
 
+// ------------reading file in sync way using  readFileSync()
+
 // console.log('Line 1');
 //  const data = fs.readFileSync('./txtFiles/input.txt');
 // console.log(data.toString());
 // console.log('Line Last')
 
-const buffer = new Buffer.alloc(1024);
-fs.open('./txtFiles/input.txt', 'r+', function(err, fd) {
-    if(err) throw err;
 
-    console.log("Reading file");
+// --------------- opeaning file fs.open() and reading file fs.read and closing file fs.close()
 
-    fs.read(fd, buffer, 0, buffer.length, 0, function(error, bytes){
-        if(error) throw error;
+// const buffer = new Buffer.alloc(1024);
+// fs.open('./txtFiles/input.txt', 'r+', function(err, fd) {
+//     if(err) throw err;
 
-        if(bytes > 0){
+//     console.log("Reading file");
 
-            console.log("Data :", buffer.slice(0, bytes).toString());
-        }
-        console.log(bytes + " Bytes read");
-    })
+//     fs.read(fd, buffer, 0, buffer.length, 0, function(error, bytes){
+//         if(error) throw error;
 
-    fs.close(fd, (err)=>{
-        if(err) throw err;
-        console.log("File closed Successfully");
-    })
+//         if(bytes > 0){
 
-})
+//             console.log("Data :", buffer.slice(0, bytes).toString());
+//         }
+//         console.log(bytes + " Bytes read");
+//     })
+
+//     fs.close(fd, (err)=>{
+//         if(err) throw err;
+//         console.log("File closed Successfully")
+//     })
+
+// })
+
+//----------- Writting in file  fs.writeFile but it will override the whole file 
+
+// fs.writeFile('./txtFiles/input.txt', "The file is updated", (err)=>{
+//     if(err) throw err;
+//     console.log('File is successfully updated');
+// })
+
+// ----- Appending in file, this will append the content in the existing content
+
+// fs.appendFile('./txtFiles/input.txt', "by Sohel", "utf-8", (err)=>{
+//     if(err) throw err;
+//     console.log('Successfully Appended');
+// })
+
+ try {
+        fs.appendFileSync('./txtFiles/input.txt', " - PW Skills");
+        console.log('success')
+ } catch (err) {
+    console.log(err)
+ }
