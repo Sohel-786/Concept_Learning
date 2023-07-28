9b5d72288a7f87e81a546843a9b253d561072861
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2023-07-28 23:39:45 +0530

    Practice

--- DSA/Two Pointer/palindrome.js
+++ DSA/Two Pointer/palindrome.js
@@ -20,3 +20,32 @@ function palindrome(str){
 }
 
 console.log(palindrome('Madama'))
+
+// brute force
+
+function palindrome2(string){
+
+    for(let j = 0 ; j < string.length ; j++){
+
+        let space = "";
+
+            for(let i = string[j].length-1 ; i>=0 ; i--){
+
+                  space  = space + string[j][i];
+
+                }
+
+    if( space == string[j] ){
+
+        console.log(string[j],"- Palindrome");
+    }
+
+    else{
+            console.log(string[j],"- Not Palindrome");
+        }
+    }
+  }
+  
+  let str = ["MOM","121","madam","Luffy"];
+  
+  palindrome2(str);
