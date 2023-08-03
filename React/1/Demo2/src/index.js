8f563ec8ecb9c268d8a8bc561eaa365c893a96bb
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2023-08-03 19:14:14 +0530

    Practice

--- React/1/Demo2/src/index.js
+++ React/1/Demo2/src/index.js
@@ -5,9 +5,21 @@ ReactDOM.render(
     <div>
         Hello <h1>Welcome</h1>
         < img src="https://e1.pxfuel.com/desktop-wallpaper/394/969/desktop-wallpaper-gojo-satoru-gojo-cute.jpg" height='200px'/>
-        <lable for="email">Enter your email</lable>
+        <lable htmlFor="email">Enter your email</lable>
         <input type='email' id='email'/>
     </div>
+
+    // /*#__PURE__*/React.createElement("div", null, "Hello ", /*#__PURE__*/React.createElement("h1", null, "Welcome"), /*#__PURE__*/React.createElement("img", {
+    // src: "https://e1.pxfuel.com/desktop-wallpaper/394/969/desktop-wallpaper-gojo-satoru-gojo-cute.jpg",
+    // height: "200px"
+    // }), /*#__PURE__*/React.createElement("lable", {
+    // htmlFor: "email"
+    // }, "Enter your email"), /*#__PURE__*/React.createElement("input", {
+    // type: "email",
+    // id: "email"
+    // }))
+
+
     ,
     document.getElementById('root')
 )
