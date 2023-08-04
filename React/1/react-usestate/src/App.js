54b0afb090cc727b3ee46a9479413e8eade09759
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2023-08-04 20:20:52 +0530

    Practice

--- React/1/react-usestate/src/App.js
+++ React/1/react-usestate/src/App.js
@@ -1,10 +1,27 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [count, setCount] = useState(0);
+
+  const addition = () => {
+    setCount(count + 1);
+  }
+  const substraction = () => {
+    setCount(count - 1);
+  }
+
   return (
-    <div>
-     
-    </div>
+    <>
+      <h2>Count : {count} </h2>
+
+      <div>
+        
+        <button onClick={addition}> + </button>
+        <button onClick={substraction}> - </button>
+      </div>
+    </>
   );
 }
 
