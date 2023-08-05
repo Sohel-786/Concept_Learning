18fafef172bf165b4fef61bea203fbbdc7e385a2
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2023-08-05 22:28:01 +0530

    Practice

--- React/1/react-conditional-rendering/my-app/src/components/Form.jsx
+++ React/1/react-conditional-rendering/my-app/src/components/Form.jsx
@@ -1,22 +1,25 @@
 import { useState } from "react"
 
-const initialState = {
-    name : "",
-    age : ""
-}
-
 export default function Form() {
 
-    const [formData, setFormdata] = useState(initialState);
+    const [formData, setFormdata] = useState({});
 
     const handleChange = (e) =>{
         console.log(e.target.name);
+
+        const {name, value} = e.target;
+
+        setFormdata({
+            ...formData,
+            [name] : value
+        })
     }
 
     return <div>
         <form>
-            <input onChange={handleChange} name="name" type="text" placeholder="Enter your name" />
-            <input onChange={handleChange} name="age" type="number" placeholder="Enter your age" />
+            <input onChange={handleChange} name="name" type="text" placeholder="Enter your Name" />
+            <input onChange={handleChange} name="age" type="number" placeholder="Enter your Age" />
+            <input onChange={handleChange} name="email" type="email" placeholder="Enter your Email" />
         </form>
     </div>
 }
