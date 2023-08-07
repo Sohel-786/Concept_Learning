3b84247bb6e199166c53382472f56a2aa07921a8
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2023-08-07 12:49:30 +0530

    Practice

--- React/1/first-vite-app/src/App.jsx
+++ React/1/first-vite-app/src/App.jsx
@@ -1,19 +1,34 @@
+
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [count, setCount] = useState(0);
+
+  function handleCouter(e){
+
+    if(count >= 0){
+      if(e.target.innerHTML === 'ADD'){
+        setCount(count + 1);
+      }else{
+        if(count > 0){
+
+          setCount(count - 1);
+        }
+        
+      }
+    }
+  }
 
   return (
     <>
-      <div className='App'>
-        <button id='btn'>
-          <span>
-            submit
-          </span>
-        </button>
+      <div >
+        <p>Count - {count}</p>
+        <div>
+          <button onClick={handleCouter}>ADD</button>
+          <button onClick={handleCouter}>SUB</button>
+        </div>
       </div>
     </>
   )
