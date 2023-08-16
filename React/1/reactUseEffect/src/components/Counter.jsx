a63cdb180933df23d89bf49b1e9bdaaa86644db6
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2023-08-16 14:59:39 +0530

    practice

--- React/1/reactUseEffect/src/components/Counter.jsx
+++ React/1/reactUseEffect/src/components/Counter.jsx
@@ -1,14 +1,21 @@
 import { useEffect, useState } from "react";
 
 function Counter(){
-    const [count, setCounter] = useState(0);
+    const [count, setCounter] = useState(10);
 
     useEffect(() =>{
 
-        setInterval(() => {
-            console.log('Change')
-            setCounter(count + 1);
-            console.log(count)
+        let clear = setInterval(() => {
+            setCounter((value) => {
+                if(value !== 0){   
+                    console.log(count);
+                    return value - 1;
+                }
+                
+                console.log(count);
+                clearInterval(clear);
+                return 0;
+            });
         }, 1000);
 
     }, []);
