b623c2f92b9968e32cfe3733de0d30595187c480
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2023-08-16 17:31:55 +0530

    Practice

--- React/1/reactUseEffect/src/components/Todo.jsx
+++ React/1/reactUseEffect/src/components/Todo.jsx
@@ -5,17 +5,17 @@ export function Todo(){
 
     const [todoList, setTodoList] = useState([]);
     const [text, setText] = useState('');
-
+    const [page, setPage] = useState(1);
 
     useEffect(() => {
 
        getTodos();
 
-    }, []);
+    }, [page]);
 
 
     function getTodos(){
-        fetch('http://localhost:3001/todos')
+        fetch(`http://localhost:3001/todos?_page=${page}&_limit=2`)
         .then((data) =>{
             return data.json()
         })
@@ -59,11 +59,11 @@ export function Todo(){
             }
 
             <button onClick={() =>{
-
+                setPage( page - 1);
             }}>Prev Page</button>
 
             <button onClick={() =>{
-                
+                setPage( page + 1);
             }}>Next Page</button>
         </>
     )
