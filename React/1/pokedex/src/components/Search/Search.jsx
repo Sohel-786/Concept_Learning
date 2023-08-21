e4ec7fe2e5b2650bf969cca39a1d8055889f9c22
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2023-08-21 20:27:39 +0530

    practice

---
+++ React/1/pokedex/src/components/Search/Search.jsx
@@ -0,0 +1,14 @@
+import './search.css'
+
+function Search() {
+    return (
+        <>
+            <input 
+                id="search-pokemon"
+                type="text"
+                placeholder="Which pokemon you are looking for ?"/>
+        </>
+    )
+}
+
+export default Search;
