1aea2dba72c1e84229cc93f4c8fb78ad309ff2ab
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2023-08-24 17:38:13 +0530

    styled-components

--- React/1/react-composition/src/components/List.jsx
+++ React/1/react-composition/src/components/List.jsx
@@ -1,8 +1,21 @@
+import styled from 'styled-components';
+
+const Li = styled.li `
+    padding: 5px 15px;
+    border-radius: 10px;
+    background-color: #97FFF4;
+    font-size: 12px;
+    font-weight: bolder;
+    color: #0c356a;
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+    border: 1px solid transparent;
+`;
+
 function List({list}) {
     return (
         <ul className="nav_list">
             {
-                list.map((el, i) => <li key={i}>{el}</li>)
+                list.map((el, i) => <Li key={i}>{el}</Li>)
             }
         </ul>
     )
