e70870c8235ead2ef607f971a89365ce1fead065
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2023-09-02 23:41:15 +0530

    practice

--- React/1/React-CustomHooks/src/App.jsx
+++ React/1/React-CustomHooks/src/App.jsx
@@ -17,14 +17,37 @@ const initialState = {
   count : 0
 }
 
+function useDebounce(cb, delay){
+
+  let debounce;
+
+  return (arg) =>{
+      debounce && clearTimeout(debounce);
+
+      debounce = setTimeout(() =>{
+          cb(arg);
+      }, delay);
+  }
+}
+
 function App() {
 
-  const [ state, dispatch ] = useReducer( reducer , initialState)
+  const [ state, dispatch ] = useReducer( reducer , initialState);
+
+  function handleChange(e){
+    console.log(e.target.value);
+  }
+
+  const debounce = useDebounce(handleChange, 300);
 
   return (
     <>
       <h1>Count : {state.count}</h1>
       <button onClick={() => dispatch({type : 'ADD_COUNT' , payload : 1 })} >ADD 1</button>
+
+      <input type="text" onChange={(e) => {
+          debounce(e);
+      }} />
     </>
   )
 }
