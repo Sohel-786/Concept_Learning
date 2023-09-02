26b240b5648fda674f0d9f79f1a708b0d7d5d33c
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2023-09-02 19:59:38 +0530

    practice

--- React/1/React-CustomHooks/src/App.jsx
+++ React/1/React-CustomHooks/src/App.jsx
@@ -1,13 +1,29 @@
-import { useState } from 'react'
+import { useReducer } from 'react';
 import './App.css'
-import GitHub from './components/GitHub'
+
+const reducer = (state, {type, payload}) => {
+  switch(type){
+
+    case "ADD_COUNT": return {
+      ...state,
+      count : state.count + payload
+    };
+    default : return { ...state }
+  }
+}
+
+const initialState = {
+  count : 0
+}
 
 function App() {
 
-
+  const [ state, dispatch ] = useReducer( reducer , initialState);
+  console.log(state);
   return (
     <>
-      <GitHub />
+      <h1> Count is : {state.count} </h1>
+      <button onClick={() => dispatch({ type : 'ADD_COUNT', payload : 1})}>Add 1</button>
     </>
   )
 }
