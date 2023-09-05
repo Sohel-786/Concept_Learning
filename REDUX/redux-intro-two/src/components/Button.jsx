ffe126b8415191c5b05c0466b67985ec3f00fe0a
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2023-09-05 09:31:40 +0530

    Practice

--- REDUX/redux-intro-two/src/components/Button.jsx
+++ REDUX/redux-intro-two/src/components/Button.jsx
@@ -1,6 +1,6 @@
-function Button({text}){
+function Button({text, className}){
     return(
-        <button className="px-3 py-2 font-mono text-gray-700 text-3xl hover:bg-gray-600 m-6 hover:text-white hover:border-2 hover:border-white hover:shadow-lg hover:shadow-cyan-900">{text}</button>
+        <button className={ className ? className :"px-3 py-2 font-mono text-gray-700 text-3xl hover:bg-gray-600 m-6 hover:text-white hover:border-2 hover:border-white hover:shadow-lg hover:shadow-cyan-900" }>{text}</button>
     )
 }
 
--- REDUX/redux-intro-two/src/components/Todos.jsx
+++ REDUX/redux-intro-two/src/components/Todos.jsx
@@ -4,38 +4,53 @@ import { addTodo } from "../Redux/actions";
 import { nanoid } from "nanoid";
 import Button from "./Button";
 
-function Todos(){
+function Todos() {
+  const [text, setText] = useState("");
+  const todos = useSelector((state) => state.todos);
+  const dispatch = useDispatch();
 
-    const [text, setText] = useState('');
-    const todos = useSelector((state) => state.todos);
-    const dispatch = useDispatch();
+  return (
+    <>
+      <form
+        className="w-7/12 flex flex-col justify-center items-center"
+        onSubmit={(e) => {
+          e.preventDefault();
+          dispatch(addTodo({ id: nanoid(), title: text, status: false }));
+        }}
+      >
+        <input
+          className="px-4 py-3 w-2/3 border-b-2 border-blue-600 focus:outline-none"
+          onChange={(e) => {
+            setText(e.target.value);
+          }}
+          type="text"
+          placeholder="Enter Your Todo"
+          value={text}
+        />
+        <Button text={"ADD"} />
+      </form>
 
-    return(
-        <>
-            <form className="w-7/12 flex flex-col justify-center items-center" onSubmit={(e) =>{
-                         e.preventDefault();
-                         dispatch(addTodo({id:nanoid() , title : text, status : false}));
-                    }}>
-                <input className="px-4 py-3 w-2/3" onChange={(e) =>{
-
-                    setText(e.target.value);
-
-                }} type="text" placeholder="Enter Your Todo" value={text}/>
-                <Button text={'ADD'} />
-            </form>
-
-            {
-                todos.map((el) => {
-                   return <div key={el.id} className="w-52 flex flex-col    justify-center items-center bg-slate-600 text-white font-mono rounded-xl"> 
-                                <h3>{el.title}</h3>
-                                <p>Status:{ el.status ? 'Completed' : 'Pending'}</p> 
-                            </div> 
-                })
-             }        
-        </>
-
-
-    )
+      {todos.map((el) => {
+        return (
+          <div
+            key={el.id}
+            className="w-3/12 text-xl shadow-md shadow-green-700 border-2 border-transparent px-8 py-9 gap-4 flex flex-col justify-center items-center bg-slate-600 text-white font-mono rounded-xl hover:border-white"
+          >
+            <h3>{el.title}</h3>
+            <p>Status:{el.status ? "Completed" : "Pending"}</p>
+            <Button
+              className={
+                el.status
+                  ? "text-white px-4 py-3 text-base bg-red-600 border-2 hover:border-white font-sans font-semibold hover:font-bold"
+                  : "bg-green-500 text-white px-4 py-3 text-base border-2 hover:border-white font-sans font-semibold hover:font-bold"
+              }
+              text={"Update Status"}
+            />
+          </div>
+        );
+      })}
+    </>
+  );
 }
 
 export default Todos;
