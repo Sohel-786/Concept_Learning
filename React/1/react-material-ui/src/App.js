9088f81b209c09074624d582e4589767353d4ca1
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2023-09-08 22:17:20 +0530

    Practice

---
+++ React/1/react-material-ui/src/App.js
@@ -0,0 +1,17 @@
+import { Button } from "@mui/material";
+import { Work } from "@mui/icons-material";
+
+function App() {
+  return (
+    <div >
+        <Button >Click</Button>
+        <Work style={
+        
+        }{
+          
+        } />
+    </div>
+  );
+}
+
+export default App;
