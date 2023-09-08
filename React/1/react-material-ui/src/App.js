fce91e3f8f6a7b547d1b87e4d9798418efb496eb
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2023-09-08 22:51:42 +0530

    Practice

--- React/1/react-material-ui/src/App.js
+++ React/1/react-material-ui/src/App.js
@@ -5,11 +5,7 @@ function App() {
   return (
     <div >
         <Button >Click</Button>
-        <Work style={
-        
-        }{
-          
-        } />
+        <Work color='primary' />
     </div>
   );
 }
