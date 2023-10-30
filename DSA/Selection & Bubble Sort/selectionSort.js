6101fd88da704c2b55a665c239c2ecc0a3a42d2d
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2023-10-30 22:09:02 +0530

    Practice

--- DSA/Selection & Bubble Sort/selectionSort.js
+++ DSA/Selection & Bubble Sort/selectionSort.js
@@ -1,10 +1,10 @@
 const arr = [64, 25, 12, 22, 11];
 const arr2 = [2, 5, 12 , 18 , -1, -4, 25, 8, -9, -103, 58, 85]
 
-function selectionSort(arr, n){
+function selectionSortAsc(arr, n){
     
     for(i= 0; i<n ; i++){
-        let minI = i;
+        minI = i;
         for(j = i+1; j<n ; j++){
             if( arr[j] < arr[minI]){
                 minI = j;
@@ -19,15 +19,38 @@ function selectionSort(arr, n){
     return arr;
 }
 
-function swap(arr, minI, i){
-    let temp = arr[i];
-    arr[i] = arr[minI];
-    arr[minI] = temp;
+
+function selectionSortDes(arr, n){
+    
+    for(i= 0; i<n ; i++){
+        maxI = i;
+        for(j = i+1; j<n ; j++){
+            if( arr[j] > arr[maxI]){
+                maxI = j;
+            }
+        }
+
+        if(maxI != i){
+            swap(arr, maxI, i);
+        }
+    }
+
+    return arr;
 }
 
-const result = selectionSort(arr, 5);
-const result2 = selectionSort(arr2, arr2.length);
+function swap(arr, change, i){
+    let temp = arr[i];
+    arr[i] = arr[change];
+    arr[change] = temp;
+}
+
+const result = selectionSortAsc(arr, 5);
+const result2 = selectionSortAsc(arr2, arr2.length);
 
 console.log(result);
 console.log(result2);
+
+console.log(selectionSortDes(arr, arr.length));
+console.log(selectionSortDes(arr2, arr2.length));
+
 
