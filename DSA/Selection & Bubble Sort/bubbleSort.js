2f681b742448d4c37990ab901c8b14067059f11f
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2023-10-31 13:23:59 +0530

    Practive

--- DSA/Selection & Bubble Sort/bubbleSort.js
+++ DSA/Selection & Bubble Sort/bubbleSort.js
@@ -1,4 +1,4 @@
-function bubbleSort(arr, n){
+function bubbleSortAsc(arr, n){
     let temp;
     let swapped;
 
@@ -24,8 +24,35 @@ function bubbleSort(arr, n){
     return arr;
 }
 
+function bubbleSortDes(arr, n){
+    let temp;
+    let swapped;
+
+    for(let i = 0; i<n ; i++){
+        swapped = false;
+        for(let j = 0; j < n - i - 1 ; j++){
+
+            if(arr[j] < arr[j+1]){
+
+                temp = arr[j+1];
+                arr[j+1] = arr[j];
+                arr[j] = temp;
+                swapped = true;
+            }
+
+        }
+
+        if(swapped == false){
+            break;
+        }
+    }
+
+    return arr;
+}
+
 const arr = [23, -1, 4, 8, 6, 33, -25, -0, 12, 28];
-console.log(bubbleSort(arr, arr.length));
+console.log(bubbleSortAsc(arr, arr.length));
+console.log(bubbleSortDes(arr, arr.length));
 
 
 
