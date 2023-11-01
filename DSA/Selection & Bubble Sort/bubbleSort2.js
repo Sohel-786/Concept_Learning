e3153b84a0bcbedf838628e2d8804c99cba0ed62
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2023-11-01 17:38:09 +0530

    Practice

---
+++ DSA/Selection & Bubble Sort/bubbleSort2.js
@@ -0,0 +1,54 @@
+// Given and Integer k, sort the values in array according to their modulo with k. That is, if there are two integers a and b, and a%k < b%k, then a would come before b in the sorted array.
+
+// if a % k = b%k, then the integer which comes first in the given array remains first in the sorted array.
+
+function Bubblesort(arr, k, n){
+    let temp;
+    for(let i = 0; i< n ;i++){
+        swapped = false;
+        for(let j = 0; j < n - i - 1 ; j++){
+            if( (arr[j]%k) > (arr[j+1]%k) ){
+                temp = arr[j+1];
+                arr[j+1] = arr[j];
+                arr[j] = temp;
+                swapped = true;
+            }
+        }
+
+        if(!swapped){
+            break;
+        }
+    }
+    arr.push('Sohel');
+    return arr;
+}
+
+function Selectionsort(arr, k, n){
+    let minI;
+
+    for(let i = 0; i < n; i++){
+        minI = i;
+        for(let j = i+1; j < n ; j++){
+
+            if((arr[i]%k) > (arr[j]%k)){
+               minI = j;
+            }
+        }
+
+        if(!(minI === i)){
+            let temp = arr[minI];
+            arr[minI] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    return arr;
+}
+
+const arr = [12, 18, 17, 65, 46];
+const k = 6;
+
+console.log(Bubblesort(arr, k, arr.length));
+console.log(arr);
+console.log(Selectionsort(arr, k, arr.length));
+console.log(arr);
