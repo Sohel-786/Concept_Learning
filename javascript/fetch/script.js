52281a3253fa7e381ced227f4257c342631ea88e
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2024-05-12 16:33:45 +0530

    Practice

--- javascript/fetch/script.js
+++ javascript/fetch/script.js
@@ -22,21 +22,33 @@
 //    }
 // }
 
-
 // https://api.openweathermap.org/data/2.5/weather?q={city name}&appid=abe99ee895c9a94800cc957300313970
 
+async function Show() {
+  try {
+    let city = document.querySelector("#city").value;
 
-async function Show(){
-
-try{
-    let city = document.querySelector('#city').value;
-
-    let res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=abe99ee895c9a94800cc957300313970`);
+    let res = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=abe99ee895c9a94800cc957300313970`
+    );
 
     let data = await res.json();
-    console.log(Math.round((data.main.temp) - 273.15), data);
-}
-catch(err){
+    console.log(Math.round(data.main.temp - 273.15), data);
+    handleContainer(data);
+  } catch (err) {
     console.log(err);
-    }
+  }
+}
+
+function handleContainer(data) {
+  let container = document.getElementById("container");
+  container.innerHTML = `<div style="color:blue;text-shadow:2px 2px; font-size:50px;font-weight : bold;">${Math.round(
+    data.main.temp - 273.15
+  )}<sup>o</sup></div> <div style="display:flex;flex-Direction:column;color: green;"><h1>${
+    data.name
+  }</h1><p style="font-weight:bold;font-size:20px;text-transform: capitalize;color:pink;">${data.weather[0].description}</p></div>`;
+
+  container.style.padding = '50px';
+  container.style.margin = '50px';
+
 }
